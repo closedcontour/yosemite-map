b50343307e50fe725cb41f7bdd7b4cb9f57df4f6
Dan Cervelli <dan@closedcontour.com>
2014-06-08 01:18:09 -0700

    Update to 0.4.

--- import-db.sh
+++ import-db.sh
@@ -1,3 +1,3 @@
 source db.sh
 
-osm2pgsql -c -d $DB -U $USER -S closedcontour.style tilemill-project/data/db/cc-yose-0.3.osm.pbf
+osm2pgsql -c -d $DB -U $USER -S closedcontour.style tilemill-project/data/db/cc-yose-0.4.osm.pbf
